9e03b355da2cb0328e96f82807bef283bfcf9eae
KevsonDoc <kevsonfilipesantos@gmail.com>
2021-05-18 18:59:47 -0300

    [add] - end

--- electron/main.js
+++ electron/main.js
@@ -1,8 +1,13 @@
 const { app } = require('electron');
+const controlWindow = require('./controlWindow');
 
 function App() {
   const win = require('./createWindow.js');
   const tray = require('./tray.js');
+
+  const { toggle } = controlWindow(win, tray);
+
+  tray.addListener('click', toggle)
 }
 
 app.whenReady().then(() => {
